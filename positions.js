73a09ca6313d217c0180f024f4320bdd4f65f29b
Işık FS Can <isikfsc@gmail.com>
2018-08-08 14:23:28 +0300

    jsut tidy up

--- positions.js
+++ positions.js
@@ -2,9 +2,9 @@ module.exports = function positions(a) {
     let pos = [];
     for(let i = 0; i < a.length; i ++) {
 	// calculate the length of prev elements in each iter
-	let temp_a = a.slice(0,i);
+	let temp_a = a.slice(0, i);
 	let temp_a_length = 0;
-	for(let j = 0; j<temp_a.length; j++) {
+	for(let j = 0; j < temp_a.length; j++) {
 	    temp_a_length += temp_a[j].length;
 	}
 	// add the commas
