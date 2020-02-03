784eba9617348094fe7e89c71b327c650edf0ef9
F. Selçuk Can <5363259+fselcukcan@users.noreply.github.com>
2020-02-03 12:02:59 +0300

    Update posit.js

--- posit.js
+++ posit.js
@@ -1,6 +1,6 @@
 // gives an array of arrays of the boundary positions of the elemensts of an array as if they were in a join()ed string
-let f = (x, i, a) => [a.slice(0, i).join("").length + i, a.slice(0,i).join("").length + i + x.length - 1];
+const f = (x, i, a) => [a.slice(0, i).join("").length + i, a.slice(0,i).join("").length + i + x.length - 1];
 
-let posit = (a) => a.map(f);
+const posit = (a) => a.map(f);
 
-module.exports = posit;
+export default posit;
