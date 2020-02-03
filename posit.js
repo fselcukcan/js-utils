d5df382e178ecfe7e702330611fb694f20800f66
F. Selçuk Can <5363259+fselcukcan@users.noreply.github.com>
2020-02-03 12:07:54 +0300

    Delete posit.js

--- posit.js
+++
@@ -1,6 +0,0 @@
-// gives an array of arrays of the boundary positions of the elemensts of an array as if they were in a join()ed string
-const f = (x, i, a) => [a.slice(0, i).join("").length + i, a.slice(0,i).join("").length + i + x.length - 1];
-
-const posit = (a) => a.map(f);
-
-export default posit;
