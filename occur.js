6890029eafc2fc7a64b709b5d1fad810efa77e78
F. Selçuk Can <5363259+fselcukcan@users.noreply.github.com>
2020-11-20 13:49:03 +0300

    Update occur.js

--- occur.js
+++ occur.js
@@ -1,1 +1,3 @@
 export const occur = (a, x) => a.reduce((acc, item) => item === x ? acc + 1 : acc, 0);
+
+/* export const occur = (a, x, test = (x, y) => x === y) => a.reduce((acc, item) => test(item, x) ? acc + 1 : acc, 0); */
