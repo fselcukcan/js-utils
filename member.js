9e3bf06ea5606f892eb8cbe97b5ada32104c841b
F. Selçuk Can <5363259+fselcukcan@users.noreply.github.com>
2021-01-01 16:16:05 +0300

    Update member.js

--- member.js
+++ member.js
@@ -1,7 +1,7 @@
-function memberAux(item, arr, {key = x => x, test = (x, y) => x === y}) {
-  return arr.some(arrElm => test(item, key(arrElm)));
-}
-
 export default function memberAux(item, arr, {key = x => x, test = (x, y) => x === y}) {
     return arr.some(arrElm => test(item, key(arrElm)));
+}
+
+function memberAux(item, arr, {key = x => x, test = (x, y) => x === y}) {
+  return arr.some(arrElm => test(item, key(arrElm)));
 }
