6594a392daabe3ac1c68f227ff7ae7f24bee73d1
F. Selçuk Can <5363259+fselcukcan@users.noreply.github.com>
2021-11-30 02:15:26 +0300

    Update range.js

--- range.js
+++ range.js
@@ -1,5 +1,5 @@
 function range(start, end, deltaAbsolute = 1) {
 	return new Array(Math.abs(Math.floor((end - start) / deltaAbsolute)))
-    .fill(start)
-    .map((x, i) => x + (i * Math.abs(deltaAbsolute) * ((end -start) / (Math.abs(end - start)))))
+    		.fill(start)
+    		.map((x, i) => x + (i * Math.abs(deltaAbsolute) * ((end -start) / (Math.abs(end - start)))))
 }
